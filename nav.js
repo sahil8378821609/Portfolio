2f280776fa07e0768735b76b90910d589c53a000
Sahil Shendkar <sahil.shendkar22@pccoepune.org>
2025-03-22 14:26:52 +0530

    first commit

---
+++ nav.js
@@ -0,0 +1,12 @@
+<script>
+    document.getElementById('navbar').innerHTML = `
+        <nav>
+            <ul>
+                <li><a href="Portfolio.html">About</a></li>
+                <li><a href="skill.html">Skills</a></li>
+                <li><a href="project.html">Projects</a></li>
+                <li><a href="CONTACT_US.html">Contact</a></li>
+            </ul>
+        </nav>
+    `;
+</script>
